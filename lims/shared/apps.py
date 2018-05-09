7b1f56a7ae86cadf962be829cecb3bba1092ec13
Thomas Craig <thomas.craig@liverpool.ac.uk>
2018-05-09 16:40:49 +0100

    Fix triggers not being fired in docker/outside of runserver

--- lims/shared/apps.py
+++ lims/shared/apps.py
@@ -8,6 +8,6 @@ class SharedConfig(AppConfig):
     name = 'lims.shared'
 
     def ready(self):
-        if 'runserver' in sys.argv or 'gunicorn' in sys.argv:
+        if 'runserver' in sys.argv or '/usr/local/bin/daphne' in sys.argv or 'runworker':
             from lims.shared.models import TriggerSet
             post_save.connect(TriggerSet()._fire_triggersets, dispatch_uid='Fire Trigger Sets')
