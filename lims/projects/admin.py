0c4f6dcb004b7a64faf93c4e8d6ebbe8404731fb
Przemysław Hendel <przem1361@gmail.com>
2018-10-29 14:31:30 +0100

    data model, registering models

--- lims/projects/admin.py
+++ lims/projects/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Project, Product, Comment, WorkLog
+from .models import Project, Product, Comment, WorkLog, Animal, StudyGroup, Container, Experiment
 
 admin.site.register(Project)
 admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(WorkLog)
+admin.site.register(Animal)
+admin.site.register(StudyGroup)
+admin.site.register(Container)
+admin.site.register(Experiment)
+
